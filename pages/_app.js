eb34677a31549019ac590f89db0713743304f8c0
Pedrito <pedroduran014@gmail.com>
2021-10-07 16:06:34 -0500

    correcciones

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import {useState,useMemo,useEffect} from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import AuthContext from '../context/AuthContext';
 import { useRouter } from "next/router";
 import jwtDecode from "jwt-decode";
@@ -6,59 +6,60 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import { setToken , getToken,removeToken} from './api/token';
+import { setToken, getToken, removeToken } from './api/token';
 
 function MyApp({ Component, pageProps }) {
 
-  const [auth, setAuth] = useState(undefined);
-  const [realoadUser, setReloadUser] = useState(false);
-  const router = useRouter();
+    const [auth, setAuth] = useState(undefined);
+    const [realoadUser, setReloadUser] = useState(false);
+    const router = useRouter();
 
-  useEffect(() => {
-    const token = getToken();
-    if (token) {
-      setAuth({
-        token,
-        idUser: jwtDecode(token).uid,
-      });
-    } else {
-      setAuth(null);
+    useEffect(() => {
+        const token = getToken();
+        if (token) {
+            setAuth({
+                token,
+                idUser: jwtDecode(token).uid,
+            });
+        } else {
+            setAuth(null);
+        }
+        setReloadUser(false);
+    }, [realoadUser]);
+
+    const login = (token) => {
+        setToken(token);
+        setAuth({
+            token,
+            idUser: jwtDecode(token).uid,
+        });
+    };
+
+    const logout = () => {
+        if (auth) {
+            removeToken();
+            setAuth(null);
+            router.push("/");
+        }
+    };
+
+
+    const authData = useMemo(
+        () => ({
+            auth,
+            login,
+            logout,
+            setReloadUser
+        }), [auth, logout]
+    );
+
+    if (auth === undefined) return null;
+    return ( <
+        AuthContext.Provider value = { authData } >
+        <
+        Component {...pageProps }
+        /> <
+        /AuthContext.Provider>)
     }
-    setReloadUser(false);
-  }, [realoadUser]);
 
-  const login = (token) => {
-    setToken(token);
-    setAuth({
-      token,
-      idUser: jwtDecode(token).uid,
-    });
-  };
-
-  const logout = () => {
-    if (auth) {
-      removeToken();
-      setAuth(null);
-      router.push("/");
-    }
-  };
-
-
-  const authData = useMemo(
-    () => ({
-      auth,
-      login,
-      logout,
-      setReloadUser
-    }),
-    [auth]
-  );
-
-  if (auth === undefined) return null;
-  return (
-  <AuthContext.Provider value={authData}>
-    <Component {...pageProps} />
-  </AuthContext.Provider>)
-}
-
-export default MyApp
+    export default MyApp
